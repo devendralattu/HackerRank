0ecc3ef450fef067365960e2d75c66ef7ddb0285
Devendra Lattu <dev12@umbc.edu>
2016-01-29 19:47:12 -0500

    optimized

--- worldcodesprint/Powerplants_In_Flatland.java
+++ worldcodesprint/Powerplants_In_Flatland.java
@@ -12,45 +12,54 @@ public class Powerplants_In_Flatland {
 		Scanner in = new Scanner(System.in);
 		int N = in.nextInt();
 		int M = in.nextInt();
-		int a[] = new int[N];
 
 		Map<Integer, Integer> b = new HashMap<Integer, Integer>();
-		
+
 		int c[] = new int[N];
 		int i = 0, ci = 0;
-		
+
 		// read space station cities
-		while (M-- > 0) {
+		if (M <= 0) {
+			System.out.println(0);
+			return;
+		}
+
+		for (i = 0; i < M; i++) {
 			int key = in.nextInt();
 			b.put(key, 0);
 		}
-		
-		// allocate cities from 0 to N - 1
-		for (i = 0; i < N; i++) {
-			a[i] = i;
-		}
+
 		// start from non space cities
 		int p1 = 0, p2 = 0;
 		for (i = 0; i < N; i++) {
 			int p1Count = 0, p2Count = 0;
 			p1 = p2 = i;
-			while (!(b.containsKey(a[p1]) || b.containsKey(a[p2]))) {
-				if (p1 > 0) {
-					p1--;
-					p1Count++;
+			boolean p1Stopped = true, p2Stopped = true;
+
+			if (b.containsKey(i)) {
+				c[i] = 0;
+			} else {
+				while (!(b.containsKey(p1) || b.containsKey(p2))) {
+					if (p1 > 0 && p1Stopped) {
+						p1--;
+						p1Count++;
+					} else {
+						p1Stopped = false;
+					}
+					if (p2 < N && p2Stopped) {
+						p2++;
+						p2Count++;
+					} else {
+						p2Stopped = false;
+					}
 				}
-				if (p2 < N) {
-					p2++;
-					p2Count++;
+				if (p1Stopped && p2Stopped) {
+					c[i] = min(p1Count, p2Count);
+				} else {
+					c[i] = max(p1Count, p2Count);
 				}
 			}
-			c[i] = min(p1Count, p2Count);
 		}
-
-		for (i = 0; i < N; i++) {
-			System.out.print(c[i] + " ");
-		}
-		System.out.println();
 
 		int max = getMax(c);
 		System.out.println(max);
@@ -69,5 +78,9 @@ public class Powerplants_In_Flatland {
 
 	public static int min(int p1Count, int p2Count) {
 		return (p1Count > p2Count) ? p2Count : p1Count;
+	}
+
+	public static int max(int p1Count, int p2Count) {
+		return (p1Count < p2Count) ? p2Count : p1Count;
 	}
 }
