b628385437bafb7dccaad80de63e3d1a8fa31354
Devendra Lattu <dev12@umbc.edu>
2016-10-14 16:51:36 -0400

    Create SumVsXOR.java

---
+++ DynamicProgramming/BitManipulation/SumVsXOR.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long num = n;
+        long count = 0;
+        while(n != 0){
+            if(n%2 == 0){
+                //last bit is 0;
+                count++;
+            }
+            n = n >> 1;
+        }
+        System.out.print((long)Math.pow(2, count));
+    }
+}
+
+/**
+Given an integer n, find x each such that:
+0 <= x <= n
+n + x = n ^ x
+where ^ denotes the bitwise XOR operator. 
+Then print an integer denoting the total number of x's satisfying the criteria above.
+**/
